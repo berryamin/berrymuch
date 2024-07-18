3b1814734a57c258fb1e35a79a100cdf1100c252
Olivier Kaloudoff <kaloudov@yandex.com>
2024-07-18 10:59:01 +0000

    python322 compiles fine except libinstall, installation fails

--- ports-wip/python322/build.sh
+++ ports-wip/python322/build.sh
@@ -12,10 +12,7 @@ source ../../lib.sh
 DISTVER="Python"
 #GITTAG="v1.43"
 DISTSUFFIX="git"
-DISTFILES="https://github.com/blackberry/$DISTVER.$DISTSUFFIX"
-
-#BUILD_DEP_BINS=()
-#check_required_binaries
+DISTFILES="https://github.com/BerryFarm/$DISTVER.$DISTSUFFIX"
 
 TASK=fetch
 package_init "$@"
@@ -27,6 +24,26 @@ CONFIGURE_CMD=" ./configure
                 CC=$PBTARGETARCH-gcc
                 "
 
+clean()
+{
+	SEARCHDIRS="Doc Grammar Include Lib Mac Misc Modules Objects Parser PC Python Tools"
+	find ${SEARCHDIRS} -name '*.[oa]' -delete
+	find ${SEARCHDIRS} -name '*.s[ol]' -delete
+	find ${SEARCHDIRS} -name '*.so.[0-9]*.[0-9]*' -delete
+	find ${SEARCHDIRS} -name '*.gc??' -delete
+	#find ${SEARCHDIRS} -name '__pycache__' -exec rm -rf {} \;
+	rm -f Lib/lib2to3/*Grammar*.pickle
+	rm -f python Parser/pgen libpython*.so* libpython*.a \
+	      tags TAGS Parser/pgen.stamp \
+	      config.cache config.log pyconfig.h Modules/config.c
+	rm -rf build platform
+	rm -rf Lib/test/data
+	rm -f core Makefile Makefile.pre config.status \
+	      Modules/Setup Modules/Setup.local Modules/Setup.config \
+	      Modules/ld_so_aix Modules/python.exp Misc/python.pc
+	rm -f python*-gdb.py
+	rm -f pybuilddir.txt
+}
 
 if [ "$TASK" == "fetch" ]
 then
@@ -41,23 +58,57 @@ fi
 
 package_patch 1
 
-if [ "$TASK" == "build" ]
+if [ "$TASK" == "patch" ]
 then
-  echo "Building"
+  echo "Patching"
   cd "$WORKDIR"
-  mv Python-3/* .
-  rm -rf Python-3
-
+ 
   for apatch in $EXECDIR/post-conf-patches/*
    do
         patch -p0 < $apatch
    done
 
-  eval $CONFIGURE_CMD
-  eval $MAKE_PREFIX make $MYMAKEFLAGS || \
-        eval $MAKE_PREFIX make
+  TASK=build
+fi
+
+if [ "$TASK" == "build" ]
+then
+  echo "Building"
+  cd "$WORKDIR"
+  mv Python-3/* .
+  rm -rf Python-3/
+
+  for TARGET in host nto-x86 nto-armv7; do
+  echo "************************************************************************"
+  echo " Building for ${TARGET}..."
+  echo "************************************************************************"
+        clean
+        ./${TARGET}.build
+        if [ $? != 0 ]; then
+                echo "Build failed!"
+                break
+        fi
+  done
+
   TASK=install
 fi
 
+
 package_install
+
+
+if [ "$TASK" == "bundle" ]
+then
+	echo "Zipping and copying to packages"
+	echo $ROOTDIR
+	cd $WORKROOT/$DISTVER/nto-armv7/accounts/1000/shared/misc/clitools
+	zip -yr $DISTVER.zip .
+	mv $DISTVER.zip $ROOTDIR/packages
+
+	echo "Moving to archive"
+	mv $WORKROOT/$DISTVER/nto-armv7/accounts $ROOTDIR/archive/
+
+	echo "Moving host to $ROOTDIR/host for future builds"
+	mv $WORKROOT/$DISTVER/host $ROOTDIR/
+fi
 package_bundle
